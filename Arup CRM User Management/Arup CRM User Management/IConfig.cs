6b43baa56065ae19e86f5fbf6c8161fb914746d3
Shruti Patil <shruti.patil@arup.com>
2020-06-16 14:02:03 +0100

    Implemeted keyvalut changes

---
+++ Arup CRM User Management/Arup CRM User Management/IConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arup_CRM_User_Management
+{
+    interface IConfig
+    {
+        string KeyVaultPath { get; }
+
+        string CRMHubPWKey { get; }
+    }
+}
